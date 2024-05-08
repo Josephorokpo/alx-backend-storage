291b1515a6d4dac85b1c7403b8fd42884a57bb61
josephorokpo <josephorokpo@gmail.com>
2024-05-08 19:51:33 +0000

    3-glam_rock.sql

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- Rank bands with Glam rock as their main style by longevity
+SELECT band_name, IFNULL(split, 2022) - IFNULL(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
