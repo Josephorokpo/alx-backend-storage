c3e96e1949defcba650d4ada7daf667d05fa53eb
josephorokpo <josephorokpo@gmail.com>
2024-05-08 19:39:52 +0000

    0-uniq_users.sql

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- Create the 'users' table with specified attributes
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
