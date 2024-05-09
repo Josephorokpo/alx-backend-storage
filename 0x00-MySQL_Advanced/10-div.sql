a60bbe8a86ae49aa0a653832da3a55448b70451b
josephorokpo <josephorokpo@gmail.com>
2024-05-09 15:16:31 +0000

    10-div.sql

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,15 +1,20 @@
--- Create a function SafeDiv that handles division by zero
+-- Script script that creates a function SafeDiv 
+-- that divides (and returns) the first by the second number 
+-- or returns 0 if the second number is equal to 0
 
-CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS DECIMAL(10, 2)
+DELIMITER $$ ;
+CREATE FUNCTION SafeDiv(
+	a INT,
+	b INT
+)
+RETURNS FLOAT
+DETERMINISTIC
 BEGIN
-    DECLARE result DECIMAL(10, 2);
-    
-    IF b = 0 THEN
-        SET result = 0;
-    ELSE
-        SET result = a / b;
-    END IF;
-    
-    RETURN result;
-END;
+	DECLARE result FLOAT;
+	IF b = 0 THEN
+		RETURN 0;
+        END IF;
+        SET result = (a * 1.0) / b;
+        RETURN result;
+END;$$
+DELIMITER ;
